9e2d23d4607f32006b13e4b1f37dc230ffd9981d
flonix8 <flstanek@googlemail.com>
2023-06-19 18:06:05 +0200

    Implement batching; Some pretty graph drawing

--- benchmark.py
+++ benchmark.py
@@ -3,11 +3,12 @@ import os
 import time
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Iterator
 
 import cv2
 import numpy as np
 import torch
+import torch.utils.data
 from typing import NamedTuple
 from google.protobuf.text_format import MessageToString
 from ultralytics.nn.autobackend import AutoBackend
@@ -18,47 +19,58 @@ from ultralytics.yolo.utils.ops import non_max_suppression, scale_boxes
 CONFIDENCE_THRESHOLD = 0.25
 IOU_THRESHOLD = 0.7
 INFERENCE_SIZE = (640, 640)
-TORCH_DEVICE = torch.device('cuda:0')
+TORCH_DEVICE = torch.device('cpu')
 YOLO_WEIGHTS = "yolov8n.pt"
+BATCH_SIZE = 10
+
+def is_cuda():
+    return TORCH_DEVICE.type == 'cuda'
+
+def is_cpu():
+    return TORCH_DEVICE.type == 'cpu'
 
 
 @dataclass
 class Metrics:
     inference_start: int = 0
-    input_preprocessing: int = 0
     model: int = 0
     non_max_suppression: int = 0
-    box_scaling: int = 0
     total_time_us: int = 0
+    batch_size: int = 0
 
 
-def frame_iter(path):
-    files = sorted(os.listdir(path))
-    for file in files:
-        basename = os.path.splitext(file)[0]
-        cv2_frame = cv2.imread(os.path.join(path, file))
-        yield basename, cv2_frame
+class FramesDataset(torch.utils.data.IterableDataset):
+    def __init__(self, path, model_stride) -> None:
+        super().__init__()
+        self.path = path
+        self.model_stride = model_stride
 
-def prepare_input(image, stride):
-    out_img = LetterBox(INFERENCE_SIZE, auto=True, stride=stride)(image=image)
-    out_img = out_img.transpose((2, 0, 1))[::-1]
-    out_img = np.ascontiguousarray(out_img)
-    out_img = torch.from_numpy(out_img).to(TORCH_DEVICE).float() / 255.0
-    return out_img.unsqueeze(0)
+    def __iter__(self) -> Iterator:
+        files = sorted(os.listdir(self.path))
+        for file in files:
+            cv2_frame = cv2.imread(os.path.join(self.path, file))
+            yield self.prepare_input(cv2_frame)
+
+    def prepare_input(self, image):
+        out_img = LetterBox(INFERENCE_SIZE, auto=True, stride=self.model_stride)(image=image)
+        out_img = out_img.transpose((2, 0, 1))[::-1]
+        out_img = np.ascontiguousarray(out_img)
+        out_img = torch.from_numpy(out_img).to(TORCH_DEVICE).float() / 255.0
+        return out_img
+    
 
 @torch.no_grad()
-def infer(model, input_image):
+def infer(model, inf_batch):
 
     metrics = Metrics()
         
     inference_start = time.monotonic_ns()
     metrics.inference_start = inference_start
 
-    inf_image = prepare_input(input_image, model.stride)
+    yolo_prediction = model(inf_batch)
 
-    metrics.input_preprocessing = time.monotonic_ns()
-
-    yolo_prediction = model(inf_image)
+    if is_cuda():
+        torch.cuda.synchronize()
 
     metrics.model = time.monotonic_ns()
 
@@ -66,15 +78,12 @@ def infer(model, input_image):
         yolo_prediction, 
         conf_thres=CONFIDENCE_THRESHOLD, 
         iou_thres=IOU_THRESHOLD
-    )[0]
+    )
 
     metrics.non_max_suppression = time.monotonic_ns()
     
-    predictions[:, :4] = scale_boxes(inf_image.shape[2:], predictions[:, :4], input_image.shape[:2]).round()
-
-    metrics.box_scaling = time.monotonic_ns()
-
     metrics.total_time_us = time.monotonic_ns()
+    metrics.batch_size = inf_batch.shape[0]
     return metrics, predictions
 
 
@@ -86,14 +95,20 @@ if __name__ == '__main__':
     )
     model.eval()
     input_image_size = check_imgsz(INFERENCE_SIZE, stride=model.stride)
+
+    if is_cpu():
+        import intel_extension_for_pytorch as ipex
+        model = ipex.optimize(model)
+
+    dataloader = torch.utils.data.DataLoader(FramesDataset('frames', model.stride), batch_size=BATCH_SIZE)
     
     with open('metrics.csv', 'w', newline='') as metrics_file:
         csv_writer = csv.DictWriter(metrics_file, fieldnames=Metrics.__dataclass_fields__)
         csv_writer.writeheader()
 
-        for basename, frame in frame_iter('frames'):
-            print(f'inferencing {basename}')
-            metrics, _ = infer(model, frame)
+        for idx, frame_batch in enumerate(dataloader):
+            print(f'inferencing batch {idx} of size {BATCH_SIZE}')
+            metrics, _ = infer(model, frame_batch)
             csv_writer.writerow(metrics.__dict__)
         
     
