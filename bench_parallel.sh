062c16b32ee92431f07c522de529fc60f24d5e5f
flonix8 <flstanek@googlemail.com>
2023-06-20 08:30:16 +0200

    Extend parallel benchmark

--- bench_parallel.sh
+++ bench_parallel.sh
@@ -1,6 +1,21 @@
-numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 1 -C 168-215 -m 1 python benchmark.py -s 48t -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_1p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+
+wait
+
+numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_2p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_2p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+
+wait
+
+numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+
+wait
+
+numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+numactl -N 1 -C 168-215 -m 1 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
 
 wait
