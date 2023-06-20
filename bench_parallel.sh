9a62cb6a0a0199b83ad7c64ad0fb189e00259d51
flonix8 <flstanek@googlemail.com>
2023-06-20 08:52:15 +0200

    Make benchmark cancellable

--- bench_parallel.sh
+++ bench_parallel.sh
@@ -1,21 +1,37 @@
-numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_1p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_1p_1 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    wait
+)
 
 wait
 
-numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_2p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_2p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_2p_1 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ & 
+    numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_2p_2 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    wait
+)
 
 wait
 
-numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_3p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_3p_1 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_3p_2 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_3p_3 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    wait
+)
 
 wait
 
-numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
-numactl -N 1 -C 168-215 -m 1 python benchmark.py -s 48t_4p -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t_4p_1 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    numactl -N 0 -C 112-159 -m 0 python benchmark.py -s 48t_4p_2 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    numactl -N 1 -C 56-103 -m 1 python benchmark.py -s 48t_4p_3 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    numactl -N 1 -C 168-215 -m 1 python benchmark.py -s 48t_4p_4 -b 10 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ &
+    wait
+)
 
 wait
