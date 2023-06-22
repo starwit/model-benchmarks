c474d6a6440314387b1335f02f396e3937458425
flonix8 <flstanek@googlemail.com>
2023-06-22 12:00:34 +0200

    Fix amx benchmark (all writing into same file)

--- bench_amx_verification.sh
+++ bench_amx_verification.sh
@@ -1,9 +1,9 @@
 numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
 
 ONEDNN_ENABLE_MAX_CPU_ISA=ON ONEDNN_MAX_CPU_ISA=AVX512_CORE_AMX \
-numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
+numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c_env -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
 
-numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c -b 8 -q bfloat16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
+numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c_bf16 -b 8 -q bfloat16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
 
 ONEDNN_ENABLE_MAX_CPU_ISA=ON ONEDNN_MAX_CPU_ISA=AVX512_CORE_AMX \
-numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c -b 8 -q bfloat16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
+numactl -N 0 -m 0 -C 0-47 python benchmark.py -s 48c_bf16_env -b 8 -q bfloat16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o amx_verification
