8029f0517dea72a682cd564ec4550ddba9989468
flonix8 <flstanek@googlemail.com>
2023-06-22 10:24:24 +0200

    Add more analysis; Add hyperthreading to parallel benchmark

--- bench_parallel.sh
+++ bench_parallel.sh
@@ -1,3 +1,4 @@
+# 14c without HT
 (
     trap 'kill 0' SIGINT;
     numactl -N 0 -m 0 -C 0-13   python benchmark.py -s 14c_4p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
@@ -20,6 +21,30 @@
     wait
 )
 
+# 14c with HT
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -m 0 -C 0-13,112-125   python benchmark.py -s 14c28t_4p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 0 -m 0 -C 14-27,126-139  python benchmark.py -s 14c28t_4p_cpu14 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 56-69,168-181  python benchmark.py -s 14c28t_4p_cpu56 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 70-83,182-195  python benchmark.py -s 14c28t_4p_cpu70 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    wait
+)
+
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -m 0 -C 0-13,112-125   python benchmark.py -s 14c28t_8p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 0 -m 0 -C 14-27,126-139  python benchmark.py -s 14c28t_8p_cpu14 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 0 -m 0 -C 28-41,140-153  python benchmark.py -s 14c28t_8p_cpu28 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 0 -m 0 -C 42-55,154-167  python benchmark.py -s 14c28t_8p_cpu42 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 56-69,168-181  python benchmark.py -s 14c28t_8p_cpu56 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 70-83,182-195  python benchmark.py -s 14c28t_8p_cpu70 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 84-97,196-209  python benchmark.py -s 14c28t_8p_cpu84 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 98-111,210-223 python benchmark.py -s 14c28t_8p_cpu98 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    wait
+)
+
+# 28c without HT
 (
     trap 'kill 0' SIGINT;
     numactl -N 0 -m 0 -C 0-27   python benchmark.py -s 28c_2p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
@@ -33,5 +58,22 @@
     numactl -N 0 -m 0 -C 28-55  python benchmark.py -s 28c_4p_cpu28 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
     numactl -N 1 -m 1 -C 56-83  python benchmark.py -s 28c_4p_cpu56 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
     numactl -N 1 -m 1 -C 84-111 python benchmark.py -s 28c_4p_cpu84 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    wait
+)
+
+# 28c with HT
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -m 0 -C 0-27,112-139   python benchmark.py -s 28c48t_2p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 56-83,168-195  python benchmark.py -s 28c48t_2p_cpu56 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    wait
+)
+
+(
+    trap 'kill 0' SIGINT;
+    numactl -N 0 -m 0 -C 0-27,112-139   python benchmark.py -s 28c_4p_cpu0  -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 0 -m 0 -C 28-55,140-167  python benchmark.py -s 28c_4p_cpu28 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 56-83,168-195  python benchmark.py -s 28c_4p_cpu56 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
+    numactl -N 1 -m 1 -C 84-111,196-223 python benchmark.py -s 28c_4p_cpu84 -b 8 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o parallel &
     wait
 )
--- bench_threads_batchsize.sh
+++ bench_threads_batchsize.sh
@@ -29,28 +29,3 @@ numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t -b 8  -i ~/workspaces/datas
 numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t -b 16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
 numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t -b 32 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
 numactl -N 0 -C 0-47 -m 0 python benchmark.py -s 48t -b 64 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 1 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 2 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 4 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 8  -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 32 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-115 python benchmark.py -s 60t -b 64 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 1 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 2 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 4 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 8  -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 32 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-127 python benchmark.py -s 72t -b 64 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 1 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 2 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 4 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 8  -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 16 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 32 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-numactl -N 0 -m 0 -C 0-55,112-139 python benchmark.py -s 84t -b 64 -i ~/workspaces/datasets/carmel/ArchWestMainStreetEB/ -o threads_batchsize
-
